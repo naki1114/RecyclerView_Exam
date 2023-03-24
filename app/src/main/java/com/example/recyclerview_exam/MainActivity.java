6277b46575c5e04727c3dbdf0684175e1280c926
Naki <nhs120417@gmail.com>
2023-03-24 14:50:11 +0900

    Recycler View 아이템 100개 생성 예제 완료

--- app/src/main/java/com/example/recyclerview_exam/MainActivity.java
+++ app/src/main/java/com/example/recyclerview_exam/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.recyclerview_exam;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    Adapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView)findViewById(R.id.recyclerViewEx);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+
+        adapter = new Adapter();
+        for (int i = 0; i < 100; i++) {
+            String str = i + "번째 아이템";
+            adapter.setArrayData(str);
+        }
+
+        recyclerView.setAdapter(adapter);
     }
 }
